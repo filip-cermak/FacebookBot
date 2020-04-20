c243f665ac3c2f79a011982b61944132d588d227
filip-cermak <fc469@cam.ac.uk>
2020-04-20 12:09:57 +0200

    adding client and service

--- Program.cs
+++ Program.cs
@@ -2,66 +2,8 @@
 
 namespace facebookBot
 {
-    class Program
+    partial class Program
     {
-        public class Account
-        {
-            public string Id { get; set; }
-            public string Name { get; set; }
-            public string Email { get; set; }
-            public string Locale { get; set; }
-            public string UserName { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Gender { get; set; }
-        }
-
-        public interface IFacebookClient
-        {
-            Task<T> GetAsync<T>(string accessToken, string endpoint, string args = null);
-            Task PostAsync(string accessToken, string endpoint, object data, string args = null);
-        }
-
-        public class FacebookClient : IFacebookClient
-{
-    private readonly HttpClient _httpClient;
-
-    public FacebookClient()
-    {
-        _httpClient = new HttpClient
-        {
-            BaseAddress = new Uri("https://graph.facebook.com/v2.8/")
-        };
-        _httpClient.DefaultRequestHeaders
-            .Accept
-            .Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    }
-
-    public async Task<T> GetAsync<T>(string accessToken, string endpoint, string args = null)
-    {
-        var response = await _httpClient.GetAsync($"{endpoint}?access_token={accessToken}&{args}");
-        if (!response.IsSuccessStatusCode)
-            return default(T);
-
-        var result = await response.Content.ReadAsStringAsync();
-
-        return JsonConvert.DeserializeObject<T>(result);
-    }
-
-    public async Task PostAsync(string accessToken, string endpoint, object data, string args = null)
-    {
-        var payload = GetPayload(data);
-        await _httpClient.PostAsync($"{endpoint}?access_token={accessToken}&{args}", payload);
-    }
-
-    private static StringContent GetPayload(object data)
-    {
-        var json = JsonConvert.SerializeObject(data);
-
-        return new StringContent(json, Encoding.UTF8, "application/json");
-    }
-}
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
